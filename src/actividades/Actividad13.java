7d735f2a1d422c07e8d3dcc049bcb49cdb92217c
carlos55ml <cmoranlozano55@gmail.com>
2021-11-05 00:02:52 +0100

    actividad 13

---
+++ src/actividades/Actividad13.java
@@ -0,0 +1,26 @@
+package actividades;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Actividad13 {  
+    public static void main(String[] args) throws IOException {
+        /* Realizar un programa que calcule el tiempo en horas y minutos que tarda un tren
+        en llegar a su destino. Para ello se le pedirán al usuario el día, hora y minuto 
+        de salida y el día, hora y minuto de llegada. */
+
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        int repetir=0;
+
+        do {
+            
+            int ds=0,hs=0,ms=0,dl=0,hl=0,ml=0;
+            
+
+        } while (repetir==0);
+
+    }
+    
+}
